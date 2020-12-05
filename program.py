8dce456ff58337056fcbdb6757cfce4c2074c2cc
Jay Kudva <jaykudva@gmail.com>
2020-12-04 22:51:47 -0800

    [ADD] added uppercase replacement

--- program.py
+++ program.py
@@ -5,4 +5,4 @@ fileToWrite = open("write.txt", "w")
 
 
 for word in data:
-    fileToWrite.write(word.replace("a", "x").replace("e", "x").replace("i", "x").replace("o", "x").replace("u", "x"))
+    fileToWrite.write(word.replace("a", "x").replace("e", "x").replace("i", "x").replace("o", "x").replace("u", "x").replace("A", "X").replace("E", "X").replace("I", "X").replace("O", "X").replace("U", "X"))
